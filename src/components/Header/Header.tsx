ff8f091d3acd5ce347676bb7d300398ef022b89c
Joonas Reinikka <joonas@fabricai.fi>
2020-10-24 23:28:42 +0300

    Make Header bigger

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,30 @@
 import React, { FunctionComponent } from 'react';
-
-import styles from './styles.module.css';
+import styled from '@emotion/styled';
 
 export interface Props {}
 
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: #d7f3fc;
+  min-height: 400px;
+
+  & h1 {
+    font-family: 'PermanentMarker', Helvetica, Arial, sans-serif;
+    font-size: 4rem;
+  }
+  & h3 {
+    font-family: 'PermanentMarker', Helvetica, Arial, sans-serif;
+  }
+`;
 const Header: FunctionComponent<Props> = (props) => {
   return (
-    <header className={styles.wrapper}>
+    <HeaderWrapper>
       <h1>Back to the Drawing Board</h1>
       <h3>— Web development, games and much more —</h3>
-    </header>
+    </HeaderWrapper>
   );
 };
 
