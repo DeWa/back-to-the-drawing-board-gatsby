cdd153781ffc5f953c25f5bd638570e87674e684
Joonas Reinikka <joonas@fabricai.fi>
2020-11-13 15:38:54 +0200

    Add initial paging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { Helmet } from 'react-helmet';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
 import MainLayout from '../layout/main';
 import PostListing from '../components/PostListing/PostListing';
@@ -11,6 +11,31 @@ export interface Props {
   data: any;
 }
 
+const Paging = () => {
+  const { currentPageNum, pageCount } = this.props.pageContext;
+  const prevPage = currentPageNum - 1 === 1 ? '/' : `/${currentPageNum - 1}/`;
+  const nextPage = `/${currentPageNum + 1}/`;
+  const isFirstPage = currentPageNum === 1;
+  const isLastPage = currentPageNum === pageCount;
+
+  return (
+    <div className="paging-container">
+      {!isFirstPage && <Link to={prevPage}>Previous</Link>}
+      {[...Array(pageCount)].map((_val, index) => {
+        const pageNum = index + 1;
+        return (
+          <Link
+            key={`listing-page-${pageNum}`}
+            to={pageNum === 1 ? '/' : `/${pageNum}/`}
+          >
+            {pageNum}
+          </Link>
+        );
+      })}
+      {!isLastPage && <Link to={nextPage}>Next</Link>}
+    </div>
+  );
+};
 const IndexPage: FunctionComponent<Props> = (props) => {
   const postEdges = props.data.allMarkdownRemark.edges;
 
@@ -21,6 +46,7 @@ const IndexPage: FunctionComponent<Props> = (props) => {
           <Helmet title={config.siteTitle} />
           <SEO />
           <PostListing postEdges={postEdges} />
+          {<Paging />}
         </div>
       </div>
     </MainLayout>
@@ -31,10 +57,12 @@ export default IndexPage;
 
 /* eslint no-undef: "off" */
 export const pageQuery = graphql`
-  query LandingQuery {
+  query LandingQuery($skip: Int!, $limit: Int!) {
     allMarkdownRemark(
       sort: { fields: [fields___date], order: DESC }
       filter: { fields: { collection: { eq: "posts" } } }
+      limit: $limit
+      skip: $skip
     ) {
       edges {
         node {
@@ -49,6 +77,7 @@ export const pageQuery = graphql`
             title
             tags
             date
+            category
             cover {
               childImageSharp {
                 fluid(quality: 90, maxWidth: 400) {
